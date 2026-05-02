14ad25304a4280a72321597fbe85a8d27a65f678
jhorex <macusijhorex25@gmail.com>
2026-05-02 15:19:47 +0800

    fix deployment

--- index.js
+++ index.js
@@ -14,13 +14,18 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-// Todo Schema
+// Schema
 const TodoSchema = new mongoose.Schema({
   text: String,
   completed: Boolean
 });
 
 const Todo = mongoose.model("Todo", TodoSchema);
+
+// Root route (optional)
+app.get("/", (req, res) => {
+  res.send("MERN Todo API is running 🚀");
+});
 
 // Routes
 app.get("/todos", async (req, res) => {
@@ -39,4 +44,8 @@ app.delete("/todos/:id", async (req, res) => {
   res.json({ message: "Deleted" });
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
