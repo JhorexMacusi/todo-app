16837c55a949fe7513041c23236fd674babfe291
jhorex <macusijhorex25@gmail.com>
2026-05-02 15:56:43 +0800

    Fix MERN structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const API = "https://todo-app-aoe6.onrender.com/todos";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  const fetchTodos = async () => {
+    const res = await axios.get(`${API}/todos`);
+    setTodos(res.data);
+  };
+
+  const addTodo = async () => {
+    await axios.post(`${API}/todos`, { text, completed: false });
+    setText("");
+    fetchTodos();
+  };
+
+  const deleteTodo = async (id) => {
+    await axios.delete(`${API}/todos/${id}`);
+    fetchTodos();
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={addTodo}>Add</button>
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo._id}>
+            {todo.text}
+            <button onClick={() => deleteTodo(todo._id)}>X</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
